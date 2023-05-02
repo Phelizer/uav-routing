02f54bf4e5b902752ef1a5522f8bd7e809b18902
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-05-02 14:55:07 +0200

    Move login endpoint to the auth controller

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  HttpStatus,
-  Post,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { User } from 'src/users/users.service';
@@ -14,4 +6,10 @@ import { User } from 'src/users/users.service';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req: { user: User }) {
+    return this.authService.login(req.user);
+  }
 }
