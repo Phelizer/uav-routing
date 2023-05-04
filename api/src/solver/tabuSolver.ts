d813588271e17fb5eb5ff607e58c31494328caf4
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-05-04 15:27:49 +0200

    Add some util functions

--- api/src/solver/tabuSolver.ts
+++ api/src/solver/tabuSolver.ts
@@ -1,4 +1,5 @@
-import { Milliseconds, Point, Solver } from './models';
+import { calculateTimeBetweenTwoPoints } from './calculateDistance';
+import { KilometersPeHour, Milliseconds, Point, Solver } from './models';
 
 export const tabuSolver: Solver = (
   pointsToObserve: Point[],
@@ -10,3 +11,57 @@ export const tabuSolver: Solver = (
   // dummy for now
   return [];
 };
+
+function generateNeighbor(source: Point[]) {}
+
+// utility:
+
+const createTimeFunc =
+  (speed: Milliseconds) => (point1: Point, point2: Point) =>
+    calculateTimeBetweenTwoPoints(point1, point2, speed);
+
+function routeIncludesAllPointsToObserve(
+  route: Point[],
+  pointsToObserve: Point[],
+) {
+  return pointsToObserve.every((point) => route.includes(point));
+}
+
+function routeStartsAndEndsInBases(route: Point[]): boolean {
+  const startsWithBase = Boolean((route[0] as any).type === 'base');
+  const endsWithBase = Boolean(
+    (route[route.length - 1] as any).type === 'base',
+  );
+
+  return startsWithBase && endsWithBase;
+}
+
+export function isValidRoute(
+  route: Point[],
+  maxFlightTime: Milliseconds,
+  speed: KilometersPeHour,
+  pointsToObserve: Point[],
+): boolean {
+  const time = createTimeFunc(speed);
+  let currentFlightTime = 0;
+  for (let i = 1; i < route.length; i++) {
+    currentFlightTime += time(route[i - 1], route[i]);
+    if ((route[i] as any).base) {
+      if (currentFlightTime > maxFlightTime) {
+        return false;
+      }
+      currentFlightTime = 0;
+    }
+  }
+
+  const inspectsAllPoints = routeIncludesAllPointsToObserve(
+    route,
+    pointsToObserve,
+  );
+
+  const startsAndEndsInBases = routeStartsAndEndsInBases(route);
+
+  const result = inspectsAllPoints && startsAndEndsInBases;
+
+  return result;
+}
