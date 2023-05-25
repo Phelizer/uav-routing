ac7262d825f47d5c8d0416e61e3f9fcea2427a86
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-05-25 20:44:56 +0200

    Revert "Set up postgres"
    This reverts commit ca10029cb99ce313ec50ff64811a6a392a64e281.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { SolverModule } from './solver/solver.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [SolverModule, AuthModule, UsersModule, DbModule],
+  imports: [SolverModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [],
 })
--- api/src/db/db.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DBService } from './db.service';
-
-@Module({
-  providers: [DBService],
-})
-export class DbModule {}
