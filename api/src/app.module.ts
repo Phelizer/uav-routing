3363a3be7f2d85a2162b166db84e3f60d08db7f4
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-05-26 12:46:14 +0200

    Revert "Revert "Set up postgres""
    This reverts commit ac7262d825f47d5c8d0416e61e3f9fcea2427a86.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { SolverModule } from './solver/solver.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [SolverModule, AuthModule, UsersModule],
+  imports: [SolverModule, AuthModule, UsersModule, DbModule],
   controllers: [AppController],
   providers: [],
 })
